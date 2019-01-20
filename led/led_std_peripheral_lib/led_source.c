48afa0c67c504684aad5c7ec6a596e7a78471449
satishkreddy <satishpokala40@gmail.com>
2019-01-20 09:40:59 +0530

    Led stdlib program source file is added

--- led/led_std_peripheral_lib/led_source.c
+++ led/led_std_peripheral_lib/led_source.c
@@ -1,74 +1,65 @@
-//STM32F407
+#include "main.h"
+#include "stm32f4xx_rcc.h"
+#include "stm32f4xx_gpio.h"
 
 
-//Memory mapping registers
+static __IO uint32_t uwTimingDelay;
+RCC_ClocksTypeDef RCC_Clocks;
 
-//clock configurations
-#define RCC_BASE_ADDR 	0x40023800
-#define	RCC_CR 					(RCC_BASE_ADDR+0x00)						//16-Bit HSE_ON
-#define	RCC_CFGR 				(RCC_BASE_ADDR+0x08)						//01 for HSE as system clock(Bits 0 and 1)
-#define RCC_AHB1ENB 		(RCC_BASE_ADDR+0x30)						//GPIOD Enable 3-Bit
+/* Private function prototypes -----------------------------------------------*/
+static void Delay(__IO uint32_t nTime);
 
-//GPIOD configurations
+/**
+  * @brief  Main program
+  */
+int main(void)
+{
+  /* SysTick end of count event each 10ms */
+  RCC_GetClocksFreq(&RCC_Clocks);
+  SysTick_Config(RCC_Clocks.HCLK_Frequency / 100);
+  
+  /* Enable the GPIOD peripheral */ 
+  GPIO_InitTypeDef my_gpio_struct;												//GPIO structure
+  
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);							//Enabling clock for GPIOD
+  
+  my_gpio_struct.GPIO_Pin = GPIO_Pin_14|GPIO_Pin_12|GPIO_Pin_13|GPIO_Pin_15;	//Enable four on board LED pins
+  my_gpio_struct.GPIO_Mode = GPIO_Mode_OUT;										//Mode of the pin IN/OUT/AF/AN
+  my_gpio_struct.GPIO_OType = GPIO_OType_PP;									//Push-Pull or Open Drain
+  my_gpio_struct.GPIO_PuPd = GPIO_PuPd_UP; 										//Pull-Up,Pull-Down,None
+  
+  GPIO_Init(GPIOD, &my_gpio_struct);											//Initialize the structure
+    	 
+  /* Infinite loop */
+  while (1)
+  {
+		GPIO_ToggleBits(GPIOD, 0xf000);											//Toggle PORT and PIN number/s value (for 16 pins)
+		Delay(100);
+  }
+}
 
-#define GPIOD_BASE_ADDR 			0x40020C00
-#define	GPIOD_MODE			(GPIOD_BASE_ADDR+0x00)					//01 for general purpose
-#define	GPIO_PUPDR			(GPIOD_BASE_ADDR+0x0C)					//01 pull-up
-#define	GPIOD_ODR				(GPIOD_BASE_ADDR+0x14)					//each bit for each pin first 16 pins are used
 
-#define REG(address) (*(unsigned int *)address)					//function poiner to the address
+//Delay function
+
+void Delay(__IO uint32_t nTime)
+{ 
+  uwTimingDelay = nTime;
+
 	
-
-
-void delay(int val)
-{
-	//Some delay function
-	int i,j;
-	for(i=0;i<val;i++)
-	{
-		j=0;
-		while(j<=2000)
-		{
-			//do nothing
-			j++;
-		}
-	}
+  while(uwTimingDelay != 0);
 }
 
-//15 -> blue
-//14 -> red
-//13 -> orange
-//12 -> green
-
-int main()
+/**
+  * @brief  Decrements the TimingDelay variable.
+  * @param  None
+  * @retval None
+  */
+void TimingDelay_Decrement(void)
 {
-	REG(RCC_CR) |= (1<<16);
-	REG(RCC_CFGR) |= (1<<0);
-	REG(RCC_AHB1ENB) |= (1<<3);
-	//12,13,14,15 pins are connected to on-board LED's
-	REG(GPIOD_MODE) |= (1<<24) | (1<<26) | (1<<28) | (1<<30);				//Enbale PULL UP for the pins
-	REG(GPIO_PUPDR)	|= (1<<24) | (1<<26) | (1<<28) | (1<<30);				//General Pupose Output mode for the pins
-	while(1)
-	{
-		//ON LED
-		REG(GPIOD_ODR) |= (1<<12);					//Setting pin high
-		delay(500);
-		REG(GPIOD_ODR) |= (1<<13);
-		delay(500);
-		REG(GPIOD_ODR) |= (1<<14);
-		delay(500);
-		REG(GPIOD_ODR) |= (1<<15);
-		delay(500);
-		
-		//OFF LED
-		REG(GPIOD_ODR) &= ~(1<<12);					//Clearing pin
-		delay(500);
-		REG(GPIOD_ODR) &= ~(1<<13);
-		delay(500);
-		REG(GPIOD_ODR) &= ~(1<<14);
-		delay(500);
-		REG(GPIOD_ODR) &= ~(1<<15);
-		delay(500);
-	}
+  if (uwTimingDelay != 0x00)
+  { 
+    uwTimingDelay--;
+  }
 }
+
 
