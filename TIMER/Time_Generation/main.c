4bef7e356efb30f707c326f7474a937e02c866d8
Pokala Satish Kumar Reddy <31512240+satishkreddy@users.noreply.github.com>
2019-08-29 07:08:26 +0530

    Add files via upload
    Timer time generator source file

--- TIMER/Time_Generation/main.c
+++ TIMER/Time_Generation/main.c
@@ -1,36 +1,64 @@
-f#include "stm32f4xx.h"
+#include "stm32f4xx.h"
 #include "stm32f4xx_rcc.h"
 #include "stm32f4xx_gpio.h"
 #include "stm32f4xx_tim.h"
 #include "main.h"
+#include "misc.h"
 
 static __IO uint32_t uwTimingDelay;
 RCC_ClocksTypeDef RCC_Clocks;
 
+GPIO_InitTypeDef GPIO_InitStruct;
+//int counter = 0;
+
 /* Private functions ---------------------------------------------------------*/
 
 
-void timer__init_fun(int Duty_cycle)
+void TIM2_IRQHandler()
 {
-    // Enable clock for Timer-2
-    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+	if(TIM_GetITStatus(TIM2,TIM_IT_Update))
+	{
+		GPIO_ToggleBits(GPIOD, 0x1000);
+	}
+	TIM_ClearITPendingBit(TIM2,TIM_IT_Update);
+}
 
-    // Timer -2 has a clock of 84MHz
-    // so update event is 1ms
-    // Update Event (Hz) = timer_clock / ((Prescaler + 1) * 
-    // (Period + 1))
-    // Event (Hz) = 84MHz / ((20999 + 1) * (99 + 1)) = 40 Hz or 1 ms
-    TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStruct;		//Timer structure pointer
-    TIM_TimeBaseInitStruct.TIM_Prescaler = 20999;
-    TIM_TimeBaseInitStruct.TIM_Period = 99;
-    TIM_TimeBaseInitStruct.TIM_ClockDivision = TIM_CKD_DIV1;
-    TIM_TimeBaseInitStruct.TIM_CounterMode = TIM_CounterMode_Up;
 
-    // TIM2 initialize
-    TIM_TimeBaseInit(TIM2, &TIM_TimeBaseInitStruct);
-    // Start TIM2
-    TIM_Cmd(TIM2, ENABLE);
 
+
+void timer__init_fun()
+{
+  // Enable clock for Timer-2
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+	
+  // Timer -2 has a clock of 84MHz
+  // so update event is 1ms
+  // Update Event (Hz) = timer_clock / ((Prescaler + 1) * 
+  // (Period + 1))
+  // Event (Hz) = 84MHz / ((8399 + 1) * (9 + 1)) = 40 Hz or 1 ms
+  TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStruct;		//Timer structure pointer
+  TIM_TimeBaseInitStruct.TIM_Prescaler = 8399;
+  TIM_TimeBaseInitStruct.TIM_Period = 999;
+  TIM_TimeBaseInitStruct.TIM_ClockDivision = TIM_CKD_DIV1;
+  TIM_TimeBaseInitStruct.TIM_CounterMode = TIM_CounterMode_Up;
+	
+  // TIM2 initialize
+  TIM_TimeBaseInit(TIM2, &TIM_TimeBaseInitStruct);
+
+	//Interrupt vector table structure initialization
+	
+	NVIC_InitTypeDef NVIC_Init_structure;
+	NVIC_Init_structure.NVIC_IRQChannel = TIM2_IRQn;
+	NVIC_Init_structure.NVIC_IRQChannelPreemptionPriority = 0;
+	NVIC_Init_structure.NVIC_IRQChannelSubPriority = 0;
+	NVIC_Init_structure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_Init_structure);
+	
+	TIM_ITConfig(TIM2,TIM_IT_Update,ENABLE);
+	
+  // Start TIM2
+  TIM_Cmd(TIM2, ENABLE);
+	
 }
 
 /**
@@ -40,7 +68,6 @@ void timer__init_fun(int Duty_cycle)
   */
 int main(void)
 { 
-	int counter = 0;
   /* SysTick end of count event each 10ms */
   RCC_GetClocksFreq(&RCC_Clocks);
   SysTick_Config(RCC_Clocks.HCLK_Frequency / 100);
@@ -56,17 +83,15 @@ int main(void)
   GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
   GPIO_Init(GPIOD, &GPIO_InitStruct);
 
+	timer__init_fun();
+
   while (1)
   {
-		if(TIM_GetCounter(TIM2)==10)
+/*		if(counter == 500)
 		{
-			counter++;
-			if(counter == 100)
-			{
-				GPIO_ToggleBits(GPIOD, 0xf000);
-			}
-		}
-			
+			GPIO_ToggleBits(GPIOD, 0x1000);
+			counter = 0;
+		}*/
 		//nothing
 	}
 }
