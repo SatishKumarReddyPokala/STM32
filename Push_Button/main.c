1cbbeaaabe7a4c443a9f9ea7f15408cd65e62b8e
Pokala Satish Kumar Reddy <31512240+satishkreddy@users.noreply.github.com>
2019-09-10 08:58:18 +0530

    Updated main.c
    Removed unwanted comments and code.

--- Push_Button/main.c
+++ Push_Button/main.c
@@ -1,30 +1,3 @@
-/**
-  ******************************************************************************
-  * @file    Project/STM32F4xx_StdPeriph_Templates/main.c 
-  * @author  MCD Application Team
-  * @version V1.8.0
-  * @date    04-November-2016
-  * @brief   Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; COPYRIGHT 2016 STMicroelectronics</center></h2>
-  *
-  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
-  * You may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at:
-  *
-  *        http://www.st.com/software_license_agreement_liberty_v2
-  *
-  * Unless required by applicable law or agreed to in writing, software 
-  * distributed under the License is distributed on an "AS IS" BASIS, 
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  *
-  ******************************************************************************
-  */
-
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "stm32f4xx_rcc.h"
@@ -33,24 +6,12 @@
 #include "stm32f4xx_syscfg.h"
 #include "misc.h"
 
-
-/** @addtogroup Template_Project
-  * @{
-  */ 
-
-/* Private typedef -----------------------------------------------------------*/
-/* Private define ------------------------------------------------------------*/
-/* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
-static __IO uint32_t uwTimingDelay;
 RCC_ClocksTypeDef RCC_Clocks;
 
-/* Private function prototypes -----------------------------------------------*/
-static void Delay(__IO uint32_t nTime);
+volatile int flag = 0;
 
 /* Private functions ---------------------------------------------------------*/
-
-volatile int flag = 0;
 	
 void EXTI_CONFIG(void)
 {
@@ -90,8 +51,7 @@ void EXTI_CONFIG(void)
 void EXTI0_IRQHandler()
 {
 	if(EXTI_GetITStatus(EXTI_Line0)){
-//		Delay(100);
-		flag =1;
+		flag ^=1;
 		EXTI_ClearITPendingBit(EXTI_Line0);
 	}
 }
@@ -130,56 +90,3 @@ int main(void)
   }
 }
 
-/**
-  * @brief  Inserts a delay time.
-  * @param  nTime: specifies the delay time length, in milliseconds.
-  * @retval None
-  */
-void Delay(__IO uint32_t nTime)
-{ 
-  uwTimingDelay = nTime;
-
-	
-  while(uwTimingDelay != 0);
-}
-
-/**
-  * @brief  Decrements the TimingDelay variable.
-  * @param  None
-  * @retval None
-  */
-void TimingDelay_Decrement(void)
-{
-  if (uwTimingDelay != 0x00)
-  { 
-    uwTimingDelay--;
-  }
-}
-
-#ifdef  USE_FULL_ASSERT
-
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t* file, uint32_t line)
-{ 
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-
-  /* Infinite loop */
-  while (1)
-  {
-  }
-}
-#endif
-
-/**
-  * @}
-  */
-
-
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
