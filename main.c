f1e87f91220f61785ae0328e476c3d00a013be11
Pokala Satish Kumar Reddy <31512240+satishkreddy@users.noreply.github.com>
2019-09-14 23:12:11 +0530

    usart

--- main.c
+++ main.c
@@ -1,99 +1,92 @@
-/**
-  ******************************************************************************
-  * @file    Project/STM32F4xx_StdPeriph_Templates/main.c 
-  * @author  MCD Application Team
-  * @version V1.8.0
-  * @date    04-November-2016
-  * @brief   Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; COPYRIGHT 2016 STMicroelectronics</center></h2>
-  *
-  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
-  * You may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at:
-  *
-  *        http://www.st.com/software_license_agreement_liberty_v2
-  *
-  * Unless required by applicable law or agreed to in writing, software 
-  * distributed under the License is distributed on an "AS IS" BASIS, 
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  *
-  ******************************************************************************
-  */
-
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "stm32f4xx_rcc.h"
 #include "stm32f4xx_gpio.h"
-#include "stm32f4xx_exti.h"
-#include "stm32f4xx_syscfg.h"
-#include "misc.h"
+#include "stm32f4xx_usart.h"
+#include "stm32f4xx_tim.h"
+#include "string.h"
 
-
-/** @addtogroup Template_Project
-  * @{
-  */ 
-
-/* Private typedef -----------------------------------------------------------*/
-/* Private define ------------------------------------------------------------*/
-/* Private macro -------------------------------------------------------------*/
-/* Private variables ---------------------------------------------------------*/
 static __IO uint32_t uwTimingDelay;
 RCC_ClocksTypeDef RCC_Clocks;
 
-/* Private function prototypes -----------------------------------------------*/
-static void Delay(__IO uint32_t nTime);
+int length =0;	//input string lenth
+char str[100];	//input string store
 
-/* Private functions ---------------------------------------------------------*/
-
-volatile int flag = 0;
-	
-void EXTI_CONFIG(void)
+//Initialization function
+void usart3_fun()
 {
-	//Enable clocks for GPIO and Sysconfig
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
+	//structure initialise
+	USART_InitTypeDef	my_usart_init;
+	GPIO_InitTypeDef	my_gpio_init;
 	
-	GPIO_InitTypeDef my_GPIO;
-	EXTI_InitTypeDef my_EXTI;
-	NVIC_InitTypeDef my_NVIC;
 	
-	my_GPIO.GPIO_Mode = GPIO_Mode_IN;
-	my_GPIO.GPIO_OType = GPIO_OType_PP;
-	my_GPIO.GPIO_Pin = GPIO_Pin_0;
-	my_GPIO.GPIO_PuPd = GPIO_PuPd_DOWN;
-	my_GPIO.GPIO_Speed = GPIO_Speed_2MHz;
+	//Enable clock for GPIO and USART
+  
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART3, ENABLE);
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
 	
-	GPIO_Init(GPIOA, &my_GPIO);
+	//Alternate Function GPIO initializations
 	
-	SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOA, EXTI_PinSource0);
+	my_gpio_init.GPIO_Pin = GPIO_Pin_10 | GPIO_Pin_11;	//10 --> Tx and 11 --> Rx
+	my_gpio_init.GPIO_Mode	=	GPIO_Mode_AF;
+	my_gpio_init.GPIO_OType = GPIO_OType_PP;
+	my_gpio_init.GPIO_PuPd = GPIO_PuPd_UP;
+	my_gpio_init.GPIO_Speed = GPIO_Speed_100MHz;
 	
-	my_EXTI.EXTI_Line = EXTI_Line0;
-	my_EXTI.EXTI_LineCmd = ENABLE;
-	my_EXTI.EXTI_Mode = EXTI_Mode_Interrupt;
-	my_EXTI.EXTI_Trigger = EXTI_Trigger_Rising;
+	//Alternate Function
 	
-	EXTI_Init(&my_EXTI);
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource10, GPIO_AF_USART3);
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource11, GPIO_AF_USART3);
 	
-	my_NVIC.NVIC_IRQChannel = EXTI0_IRQn;
-	my_NVIC.NVIC_IRQChannelCmd = ENABLE;
-	my_NVIC.NVIC_IRQChannelPreemptionPriority = 0x00;
-	my_NVIC.NVIC_IRQChannelSubPriority = 0x00;
+	//USART Base Initialisation
+
+	my_usart_init.USART_BaudRate = 9600;
+	my_usart_init.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	my_usart_init.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;
+	my_usart_init.USART_Parity = USART_Parity_No;
+	my_usart_init.USART_StopBits = USART_StopBits_1;
+	my_usart_init.USART_WordLength = USART_WordLength_8b;
 	
-	NVIC_Init(&my_NVIC);
+	//Init functions
+	
+	GPIO_Init(GPIOB, &my_gpio_init);
+	USART_Init(USART3, &my_usart_init);
+	
+	//ENABLE Peripheral
+	
+	USART_Cmd(USART3, ENABLE);
+	
 }
 
-void EXTI0_IRQHandler()
+
+/*function to sending a character	*/
+
+void USART3_send_char(char ch)
 {
-	if(EXTI_GetITStatus(EXTI_Line0)){
-//		Delay(100);
-		flag =1;
-		EXTI_ClearITPendingBit(EXTI_Line0);
+	//checking for flag
+	while (!USART_GetFlagStatus(USART3, USART_FLAG_TXE));
+	//send character
+	USART_SendData(USART3, (int )ch);
+}
+
+/*	function to sending a string	*/
+
+void USART3_string(char *str)
+{
+	while(*str)
+	{
+		USART3_send_char(*str++);
 	}
+	memset(str,'\0',length);
+	length = 0;
+}
+
+//get from terminal
+
+char USART_GetChar()
+{
+    while (!USART_GetFlagStatus(USART3, USART_FLAG_RXNE));
+    return USART_ReceiveData(USART3);
 }
 
 /**
@@ -102,45 +95,33 @@ void EXTI0_IRQHandler()
   * @retval None
   */
 int main(void)
-{ 
+{
+	char ch;
+	int count =0;
   /* SysTick end of count event each 10ms */
   RCC_GetClocksFreq(&RCC_Clocks);
   SysTick_Config(RCC_Clocks.HCLK_Frequency / 100);
+	uint16_t var = '\0';
 	
-	EXTI_CONFIG();
-	
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
-	
-	GPIO_InitTypeDef my_gpiod;
-	
-	my_gpiod.GPIO_Mode = GPIO_Mode_OUT;
-	my_gpiod.GPIO_OType = GPIO_OType_PP;
-	my_gpiod.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
-	my_gpiod.GPIO_PuPd = GPIO_PuPd_NOPULL;
-	my_gpiod.GPIO_Speed = GPIO_Speed_50MHz;
-	
-	GPIO_Init(GPIOD, &my_gpiod);
-	
+	usart3_fun();
+		
   /* Infinite loop */
-  while (1){
-		while(flag){
-		GPIO_ToggleBits(GPIOD,0XF000);
-			flag = 0;
+  while (1)
+  {
+		ch = USART_GetChar();	//waiting for a character
+		USART3_send_char(ch);	//printing/sending that character to terminal. this line commented data won't display on screen but it will be stored in str array
+		str[count] = ch;			//storing the character.
+		count++;							//storing data length
+		if (ch== 0x0D)				//checking for the enter ascii then printing complete string in next line and going to next line for fresh entry
+		{
+			USART3_send_char('\n');
+			USART3_string(str);
+			USART3_send_char('\n');
+			memset(str,var,count);	//resetting memory with NULL characters
+			count = 0;
+			USART3_string("success\n\r");
 		}
   }
-}
-
-/**
-  * @brief  Inserts a delay time.
-  * @param  nTime: specifies the delay time length, in milliseconds.
-  * @retval None
-  */
-void Delay(__IO uint32_t nTime)
-{ 
-  uwTimingDelay = nTime;
-
-	
-  while(uwTimingDelay != 0);
 }
 
 /**
@@ -155,31 +136,3 @@ void TimingDelay_Decrement(void)
     uwTimingDelay--;
   }
 }
-
-#ifdef  USE_FULL_ASSERT
-
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t* file, uint32_t line)
-{ 
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-
-  /* Infinite loop */
-  while (1)
-  {
-  }
-}
-#endif
-
-/**
-  * @}
-  */
-
-
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
