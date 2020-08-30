f589f6701dd7a458bbf27d87b8d83d5faf5398a9
Pokala Satish Kumar Reddy <31512240+satishkreddy@users.noreply.github.com>
2020-08-30 18:03:50 +0530

    Update main.c

--- TIMER/Time_Generation/main.c
+++ TIMER/Time_Generation/main.c
@@ -28,22 +28,22 @@ void TIM2_IRQHandler()
 
 void timer__init_fun()
 {
-  // Enable clock for Timer-2
-  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+ 	// Enable clock for Timer-2
+ 	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
 	
-  // Timer -2 has a clock of 84MHz
-  // update event is 2 Seconds
-  // Update Event (Hz) = timer_clock / ((Prescaler + 1) * 
-  // (Period + 1))
-  // Event (Hz) = 84MHz / ((41999 + 1) * (3999 + 1)) = 2 seconds
-  TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStruct;		//Timer structure pointer
-  TIM_TimeBaseInitStruct.TIM_Prescaler = 41999;
-  TIM_TimeBaseInitStruct.TIM_Period = 3999;
-  TIM_TimeBaseInitStruct.TIM_ClockDivision = TIM_CKD_DIV1;
-  TIM_TimeBaseInitStruct.TIM_CounterMode = TIM_CounterMode_Up;
+ 	// Timer -2 has a clock of 84MHz
+	// update event is 2 Seconds
+	// Update Event (Hz) = timer_clock / ((Prescaler + 1) * (Period + 1))
+	// Event (Hz) = 84MHz / ((41999 + 1) * (3999 + 1)) = 2 seconds
+	TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStruct;		//Timer structure pointer
+	TIM_TimeBaseInitStruct.TIM_Prescaler = 41999;
+	TIM_TimeBaseInitStruct.TIM_Period = 3999;
+	TIM_TimeBaseInitStruct.TIM_ClockDivision = TIM_CKD_DIV1;
+	TIM_TimeBaseInitStruct.TIM_CounterMode = TIM_CounterMode_Up;
 	
-  // TIM2 initialize
-  TIM_TimeBaseInit(TIM2, &TIM_TimeBaseInitStruct);
+	// TIM2 initialize
+	TIM_TimeBaseInit(TIM2, &TIM_TimeBaseInitStruct);
+	
 
 	//Interrupt vector table structure initialization
 	
@@ -56,8 +56,8 @@ void timer__init_fun()
 	
 	TIM_ITConfig(TIM2,TIM_IT_Update,ENABLE);
 	
-  // Start TIM2
-  TIM_Cmd(TIM2, ENABLE);
+	// Start TIM2
+	TIM_Cmd(TIM2, ENABLE);
 	
 }
 
@@ -75,7 +75,6 @@ int main(void)
   RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
 
   // initialization green LED GPIOD12
-  GPIO_InitTypeDef GPIO_InitStruct;
   GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
   GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
   GPIO_InitStruct.GPIO_Pin = GPIO_Pin_12;
@@ -83,36 +82,11 @@ int main(void)
   GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
   GPIO_Init(GPIOD, &GPIO_InitStruct);
 
-	timer__init_fun();
+	timer__init_fun();	//Timer initialization function call.
 
   while (1)
   {
-  	
-  }
-}
-
-/**
-  * @brief  Inserts a delay time.
-  * @param  nTime: specifies the delay time length, in milliseconds.
-  * @retval None
-  */
-void Delay(__IO uint32_t nTime)
-{ 
-  uwTimingDelay = nTime;
-
-	
-  while(uwTimingDelay != 0);
-}
-
-/**
-  * @brief  Decrements the TimingDelay variable.
-  * @param  None
-  * @retval None
-  */
-void TimingDelay_Decrement(void)
-{
-  if (uwTimingDelay != 0x00)
-  { 
-    uwTimingDelay--;
+  	//Do nothing
+	//Blink code is written in interrupt handler
   }
 }
